1fe6ed1ab922aff115711d9fe62b484db1aeb75f
Muthamizh23 <158249272+Muthamizh23@users.noreply.github.com>
2024-04-09 22:26:28 +0530

    Add files via upload

---
+++ src/components/productlist.jsx
@@ -0,0 +1,80 @@
+const navigationLinks = [
+  { text: "Home", url: "#" },
+  { text: "About", url: "#" },
+  {
+    text: "Shop",
+    url: "#",
+    subLinks: [
+      { text: "All Products", url: "#" },
+      { text: "Popular Items", url: "#" },
+      { text: "New Arrivals", url: "#" },
+    ],
+  },
+];
+
+const product_list = [
+  {
+      
+    img: "https://m.media-amazon.com/images/I/81WlQBd4YdL._AC_UL320_.jpg",
+    title: "berge",
+    id:1.5,
+    rating:3,
+    newprice: 20.0,
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/71q-01HbzXL._AC_UL320_.jpg",
+    title: "Keri Perry",
+    id:2,
+    rating:5,
+    sale: "Sale",
+    newprice: 10.0,
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/61-vuffz9-L._AC_UL320_.jpg",
+    title: "Grecilooks",
+    id:3,
+    rating:5,
+    sale: "Sale",
+    newprice: 8.0,
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/61ULbC6EFHL._AC_UL320_.jpg",
+    title: "Fabricorn",
+    id:4,
+    rating:2,
+    newprice: 7.0,
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/71ZqIMS9USL._AC_UL320_.jpg",
+    title: "Leriya Fashion",
+    id:5,
+    rating:3,
+    sale: "Sale",
+    newprice: 20.0,
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/71LMGs6dccL._AC_UL320_.jpg",
+    title: "Lymio",
+    id:6,
+    rating:1,
+    newprice: 10.0,
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/71q-01HbzXL._AC_UL320_.jpg",
+    title: "Dhurvi Trendz",
+    id:7,
+    rating:5,
+    sale: "Sale",
+    newprice: 13.0,
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/81WlQBd4YdL._AC_UL320_.jpg",
+    title: "lymio",
+    id:8,
+    rating:4,
+    newprice: 5.0,
+  },
+  
+  
+];
+export { product_list, navigationLinks };
